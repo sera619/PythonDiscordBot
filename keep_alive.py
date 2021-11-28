b58c7eb6a0862fd216f830e5cfbd63ea2c8b708a
S3R43o3 <67480273+sera619@users.noreply.github.com>
2021-11-28 21:47:35 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from threading import Thread
+from main import main
 # simpler webserver zum aktiv halten des bots
 
 
@@ -16,16 +17,21 @@ def home():
 </header>
 
 <body>
-  <div class="block">
-    <h1>
-      Python Discord-Bot © S3R43o3
-    </h1>
-  </div>
-  <div class="container">
-    <p class="text">
-
-    </p>
-  </div>
+  <center>
+    <div class="block">
+      <h1>
+        Python Discord-Bot © S3R43o3
+      </h1>
+    </div>
+    
+    <div class="container">
+      <ul>
+        <li>Bot-Name: {main.user.name}</li>
+        <li>Bot-ID: {main.user.id}</li>
+        <li>Bot-Server: {main.guild.name}</li>
+      </ul>
+    </div>
+  </center>
 </body>
 <footer>
 
