03bfb7914768c9e0ed4d5e2db80aa1520025353a
S3R43o3 <67480273+sera619@users.noreply.github.com>
2021-11-28 20:38:50 +0100

    Create commands.py

---
+++ commands.py
@@ -0,0 +1,12 @@
+import os
+import discord
+from discord.ext import commands
+
+TOKEN = os.environ['TOKEN']
+bot = commands.Bot(command_prefix='.')
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+    
+bot.run()
