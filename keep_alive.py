1e906ebd68b9e7ed5368551af44e6da9734d74f0
S3R43o3 <67480273+sera619@users.noreply.github.com>
2021-11-28 23:10:47 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 from threading import Thread
 
 # simpler webserver zum aktiv halten des bots
@@ -11,10 +12,26 @@ user_id: str
 
 
 @app.route('/')
-@app.route('/index')
 def home():
     return'''
 <!DOCTYPE html>
+
+<head>
+  <meta charset="utf-8">
+  <title>Python Discord-Bot by © S3R43o3</title>
+  <style>
+    html{
+      background-color: darkslategrey;
+      color: darkred;
+    }
+    .color {
+      width:80%;
+      border: 1px solid black;
+      margin: 5;
+      display: block;
+    }
+  </style>
+</head>
 
 <header>
   <nav class="navbar">
@@ -22,13 +39,16 @@ def home():
 </header>
 <body>
   <center>
-    <div class="block">
+    <div>
       <h1>
         Python Discord-Bot © S3R43o3
       </h1>
+    </div>
+    {%for i in range(times): %}
+    <div class="color">
       <p>Bot ist vollständig initialsiert.</p>
     </div>
-    
+    {% endfor %}
     <div class="container">
       <!--
       <ul>
@@ -46,6 +66,9 @@ def home():
 
 </html>'''
 
+@app.route('/index')
+def index():
+  return render_template()
 
 def run():
     app.run(host='0.0.0.0', port=8080)
