88856a290f229322063b5130baa32f246f182f58
S3R43o3 <67480273+sera619@users.noreply.github.com>
2021-11-28 22:15:11 +0100

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -2,21 +2,18 @@ from flask import Flask
 from threading import Thread
 
 # simpler webserver zum aktiv halten des bots
-user_id = 'user id'
-user_name = 'user name'
-server_name = 'server name'
+
+
+
 app = Flask('')
 
+user_id: str
 
-
-def change_bot_stats(servername, userid, username):
-  user_id = userid
-  user_name = username
-  server_name = servername
 
 @app.route('/')
 def home():
-    return '''
+  return
+'''
 <!DOCTYPE html>
 
 <header>
@@ -32,11 +29,13 @@ def home():
     </div>
     
     <div class="container">
+      <!--
       <ul>
-        <li>Bot-Name: {user_name}</li>
-        <li>Bot-ID: {user_id}</li>
-        <li>Bot-Server: {server_name}</li>
+        <li>Bot-Name: </li>
+        <li>Bot-ID: </li>
+        <li>Bot-Server: </li>
       </ul>
+      -->
     </div>
   </center>
 </body>
